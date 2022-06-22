aa2d1f5260abf9f625e23eed241287b6d568374e
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-06-22 21:36:42 +0300

    убрал лайки, не надо выпендриваться

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-const likes = document.querySelector('.elements__wrap')
 const popupClose = document.querySelector('.popup__close-icon')
 const popup = document.querySelector('.popup')
 const editButton = document.querySelector('.profile__edit-button')
@@ -7,17 +6,6 @@ const profileInfo = document.querySelector('.profile__user-info')
 const formElement = document.querySelector('.popup__container')
 const nameInput = document.querySelector('.popup__input_type_name')
 const infoInput = document.querySelector('.popup__input_type_info')
-
-likes.addEventListener('click', function(evt) {
-  if (!evt.target.classList.contains('elements__like')) return
-  if (evt.target.classList.contains('liked')) {
-    evt.target.classList.remove('liked')
-    evt.target.src="./images/like.svg"
-  } else {
-    evt.target.src="./images/like_clicked.svg"
-    evt.target.classList.add('liked')
-  }
-})
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
