10a50dc69382f80448961c89f864c70992e9ec02
Ivan Vasilyev <nalivan3747@yandex.ru>
2022-06-24 08:04:43 +0300

    fixed index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,29 @@
-const popupClose = document.querySelector('.popup__close-icon')
-const popup = document.querySelector('.popup')
-const editButton = document.querySelector('.profile__edit-button')
-const profileName = document.querySelector('.profile__user-name')
-const profileInfo = document.querySelector('.profile__user-info')
-const formElement = document.querySelector('.popup__container')
-const nameInput = document.querySelector('.popup__input_type_name')
-const infoInput = document.querySelector('.popup__input_type_info')
+const closeIcon = document.querySelector('.popup__close-icon');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__user-name');
+const profileInfo = document.querySelector('.profile__user-info');
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__input_type_name');
+const infoInput = document.querySelector('.popup__input_type_info');
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileName.textContent = nameInput.value
-    profileInfo.textContent = infoInput.value
-    popup.classList.remove('popup_opened')
+    profileName.textContent = nameInput.value;
+    profileInfo.textContent = infoInput.value;
+    popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 editButton.addEventListener('click', function() {
-  popup.classList.add('popup_opened')
-  nameInput.value = profileName.textContent
-  infoInput.value = profileInfo.textContent
-})
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  infoInput.value = profileInfo.textContent;
+});
 
-popupClose.addEventListener('click', function() {
-  popup.classList.remove('popup_opened')
-})
+closeIcon.addEventListener('click', popupClose);
