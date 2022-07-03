bf266d654edda8b1aad48149a20e31f2bbe17aa9
Ivan Vasilyev <Nalivan3747@yandex.ru>
2022-07-03 16:07:06 +0300

    feat/добавить картинки через js

--- scripts/index.js
+++ scripts/index.js
@@ -6,32 +6,81 @@ const profileInfo = document.querySelector('.profile__user-info');
 const formElement = document.querySelector('.form');
 const nameInput = document.querySelector('.form__input[name="name"]');
 const infoInput = document.querySelector('.form__input[name="info"]');
-const cards = document.querySelector('.elements__container')
+const cards = document.querySelector('.elements__container');
 
 function toggleLikes(event) {
-  if (!event.target.classList.contains('card__like')) return
-  event.target.classList.toggle('card__like_active')
+  if (!event.target.classList.contains('card__like')) return;
+  event.target.classList.toggle('card__like_active');
 }
 
-cards.addEventListener('click', toggleLikes)
+cards.addEventListener('click', toggleLikes);
 
-function popupClose() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileInfo.textContent = infoInput.value;
-    popupClose();
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = infoInput.value;
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-editButton.addEventListener('click', function() {
-  popup.classList.add('popup_opened');
+editButton.addEventListener('click', function () {
+  openPopup();
   nameInput.value = profileName.textContent;
   infoInput.value = profileInfo.textContent;
 });
 
-closeIcon.addEventListener('click', popupClose);
+closeIcon.addEventListener('click', closePopup);
+
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/karachaev.jpg',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus.jpg',
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombay.jpg',
+  },
+  {
+    name: 'Ибица',
+    link: './images/ibiza.jpg',
+  },
+  {
+    name: 'Париж',
+    link: './images/paris.jpg',
+  },
+  {
+    name: 'Венеция',
+    link: './images/venice.jpg',
+  },
+];
+
+function addCards(card) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  const newImage = newCard.querySelector('.card__image');
+  newImage.src = card.link;
+  newImage.alt = card.name;
+  newCard.querySelector('.card__caption').textContent = card.name;
+  cards.append(newCard);
+}
+
+function addDefaultCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    addCards(cards[i]);
+  }
+}
+
+addDefaultCards(initialCards);
